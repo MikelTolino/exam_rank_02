ed6f9aa84172e9cba26ff9a573fb0a9f0fe33ae5
JavierCervilla <javier@cervilla.es>
2020-08-03 23:41:18 +0200

    limpiando el gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,124 +3,107 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmateo-t <mmateo-t@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jcervill <jcervill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/29 10:59:18 by mmateo-t          #+#    #+#             */
-/*   Updated: 2020/01/30 10:17:20 by mmateo-t         ###   ########.fr       */
+/*   Created: 2020/02/19 17:57:50 by jcervill          #+#    #+#             */
+/*   Updated: 2020/07/30 23:56:48 by jcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-unsigned int ft_strlen(char *s)
+int					ft_strlen(char *s)
 {
-	unsigned int i;
+	int				len;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
 
-int	ft_strchr(char *str, int c)
+int					ft_strchr(char *s, int c)
 {
-	unsigned int i; 
-	unsigned int len;
+	int				len;
 
-	i = 0;
-	if (!str)
+	if (!s)
 		return (0);
-	len = ft_strlen(str);
-	while (str[i] && str[i] != c)
-	{
-		i++;
-	}
-	if (str[i] != c && i == len)
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	if (s[len] != c && len == ft_strlen(s))
 		return (0);
 	return (1);
 }
 
-char *ft_strdup(char *str)
+char				*ft_strdup(char *s)
 {
-	int len;
-	char *str2;
+	int				len;
+	char			*str;
 
+	len = ft_strlen(s);
+	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
 	len = 0;
-	while (str[len])
+	while (s[len])
 	{
+		str[len] = s[len];
 		len++;
 	}
-	str2 = (char *)malloc(sizeof(char) * (len + 1));
+	str[len] = 0;
+	return (str);
+}
+
+char				*ft_strjoin(char *s1, char *s2)
+{
+	int				len;
+	char			*str;
+	int				i;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
 	len = 0;
-	while (str[len])
+	while (s1[len])
 	{
-		str2[len] = str[len];
+		str[len] = s1[len];
 		len++;
 	}
-	str2[len] = '\0';
-	return (str2);
-}
-
-char *ft_strjoin(char *str1, char *str2)
-{
-	int i;
-	int j;
-	char *str3;
-
 	i = 0;
-	j = 0;
-	while (str1[i])
+	while (s2[i])
 	{
+		str[len + i] = s2[i];
 		i++;
 	}
-	while (str2[j])
-	{
-		j++;
-	}
-	str3 = (char *)malloc(sizeof(char) * (i + j + 1));
-	i = 0;
-	while (str1[i])
-	{
-		str3[i] = str1[i];
-		i++;
-	}
-	j = 0;
-	while (str2[j])
-	{
-		str3[i + j] = str2[j];
-		j++;
-	}
-	str3[i + j] = '\0';
-	return (str3);
+	str[len + i] = 0;
+	return (str);
 }
 
-char *ft_substr(char *str, int start, int len)
+char				*ft_substr(char *s, int start, int len)
 {
-	int i;
-	char *str2;
+	int				i;
+	char			*str;
 
-	str2 = (char *)malloc(sizeof(char) * (len + 1));
+	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
 	i = 0;
 	while (len--)
 	{
-		str2[i] = str[start + i];
+		str[i] = s[start + i];
 		i++;
 	}
-	str2[i] = '\0';
-	return (str2);
+	str[i] = 0;
+	return (str);
 }
 
-
-int read_line(char **line, char **s)
+int					read_line(char **line, char **s)
 {
-	int len;
-	char *aux;
+	int				len;
+	char			*aux;
 
 	len = 0;
-	while ((*s)[len] != '\0' && (*s)[len] != '\n')
-	{
+	while ((*s)[len] && (*s)[len] != '\n')
 		len++;
-	}
 	if ((*s)[len] == '\n')
 	{
 		*line = ft_substr(*s, 0, len);
@@ -138,23 +121,23 @@ int read_line(char **line, char **s)
 	return (1);
 }
 
-int get_next_line(char **line)
+int					get_next_line(char **line)
 {
-	int n_bytes;
-	static char *s;
-	char buff[BUFFER_SIZE + 1];
-	char *aux;
+	int				n_bytes;
+	char			buff[BUFF_SIZE + 1];
+	char			*aux;
+	static char		*s;
 
 	if (!line)
 		return (-1);
 	while (ft_strchr(s, '\n') == 0)
 	{
-		n_bytes = read(0, buff, BUFFER_SIZE);
+		n_bytes = read(0, buff, BUFF_SIZE);
 		if (n_bytes < 0)
 			return (-1);
 		if (n_bytes == 0)
-			break;
-		buff[n_bytes] = '\0';
+			break ;
+		buff[n_bytes] = 0;
 		if (!s)
 			s = ft_strdup(buff);
 		else
@@ -170,5 +153,5 @@ int get_next_line(char **line)
 		return (0);
 	}
 	else
-		return (read_line(line, &s));
+		return(read_line(line, &s));
 }
